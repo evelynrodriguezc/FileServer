4ae5375f080fdb176b77ee72d40268903faba155
evelynrodriguezc <evelynrodriguezc0@gmail.com>
2022-07-14 23:23:25 -0500

    using just 1 file for the server and 1 file for the client

--- client.go
+++ client.go
@@ -1,26 +1,53 @@
 package main
-
 import (
-	"fmt"
+	"strings"
 	"net"
+	"fmt"
+	"os"
 )
 
-func client() {
-	c, err := net.Dial("tcp", ":7777")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	msg := "Hola mundo"
-	fmt.Println(msg)
-	c.Write([]byte(msg))
-	c.Close()
-
-}
+const (
+    HOST = "localhost"
+    PORT = "80"
+    TYPE = "tcp"
+)
 
 func main() {
-	go client()
 
-	var input string
-	fmt.Scanln(&input)
+	argumentos := os.Args[1:]
+	commaSeparatedMessage := joinMessage(argumentos)
+
+	reply := sendMessageTo(HOST, PORT, commaSeparatedMessage)
+	fmt.Println(reply)
+}
+
+func joinMessage(arrayMessage []string) string{
+	return strings.Join(arrayMessage, ",")
+}
+
+func splitMessage(commaSeparatedMessage string)[]string{
+	return strings.Split(commaSeparatedMessage, ",")
+}
+
+func sendMessageTo(host string, port string, message string) string{
+	conn, err := net.Dial("tcp", host+":"+port) // intento de conneccion al host
+	if err != nil {
+        println("Dial failed:", err.Error())
+        os.Exit(1)
+    }
+	_, err = conn.Write([]byte(message)) //envio el mensaje
+	if err != nil {
+        println("Write to server failed:", err.Error())
+        os.Exit(1)
+    }
+
+	respuesta := make([]byte, 1024) //buffer para respuesta
+	_, err = conn.Read(respuesta) //leo la respuesta del servidor
+	if err != nil {
+        println("Response from server failed:", err.Error())
+        os.Exit(1)
+    }
+
+	conn.Close() //cerrar la connecion
+	return string(respuesta) //respuesta de el servidor
 }
--- server.go
+++ server.go
@@ -1,42 +1,69 @@
 package main
-
 import (
-	"fmt"
-	"net"
+	"strings"
+	"log"
+    "net"
+    "os"
 )
 
-func server() {
-	s, err := net.Listen("tcp", ":7777")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	for {
-		c, err := s.Accept()
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		go handleClient(c)
-	}
-}
+const (
+    HOST = "localhost"
+    PORT = "80"
+    TYPE = "tcp"
+)
 
-func handleClient(c net.Conn) {
-	b := make([]byte, 100)
-	bs, err := c.Read(b)
-	if err != nil {
-		fmt.Println(err)
-		return
-	} else {
-		fmt.Println("Mensaje: ", b[:bs])
-		fmt.Println("Bytes: ", bs)
-	}
-
-}
 
 func main() {
-	go server()
+    listen, err := net.Listen(TYPE, HOST+":"+PORT)
 
-	var input string
-	fmt.Scanln(&input)
+	channels := make(map[string][]string)
+	
+    if err != nil {
+        log.Fatal(err)
+        os.Exit(1)
+    }
+    // close listener
+    defer listen.Close()
+    for {
+		response := ""
+		conn, message := waitForActions(listen)
+		opcion := splitMessage(message)
+		println("user: " + opcion[1])
+		switch opcion[0] {
+		case "sub":
+			channels[opcion[2]] = append(channels[opcion[2]], opcion[1])
+			response = opcion[1]+" subscribed to "+opcion[2]
+		case "chans":
+			response = "getchannels"
+		case "upload":
+			response = "upload"
+		case "receive":
+			response = "download"
+		}
+
+		conn.Write([]byte(response+"\n"))
+		conn.Close()
+    }
+}
+
+func joinMessage(arrayMessage []string) string{
+	return strings.Join(arrayMessage, ",")
+}
+
+func splitMessage(commaSeparatedMessage string)[]string{
+	return strings.Split(commaSeparatedMessage, ",")
+}
+
+func waitForActions(escucha net.Listener) (net.Conn, string){
+	conn, err := escucha.Accept()
+	buffer := make([]byte, 1024)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+	_, err = conn.Read(buffer)
+	if err != nil {
+        log.Fatal(err)
+    }
+	return conn, string(buffer)
 }
